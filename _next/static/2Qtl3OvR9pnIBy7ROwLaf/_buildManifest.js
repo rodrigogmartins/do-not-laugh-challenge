3ab8c6ec63ec93dd6e2d722be3995e7d77f8c9f9
rodrigogmartins <rodrigo.martins7121@gmail.com>
2022-09-29 00:58:51 +0000

    Deploying to gh-pages from @ rodrigogmartins/do-not-laugh-challenge@ad37a40e4bd77abf9038b870dbb1004a72f689e7 🚀

---
+++ _next/static/2Qtl3OvR9pnIBy7ROwLaf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/217-bbfc6d9ff7157fe9.js","static/css/9f3e31cb3bbbcca6.css","static/chunks/pages/index-e7fafb1dcc5313b0.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
